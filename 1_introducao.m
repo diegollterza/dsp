21d1adf81c44fc87abeb1ed0756e5d07f0200195
Luis Felipe Mileo <mileo@kmee.com.br>
2018-12-09 10:27:15 -0200

    [NEW] Introducao
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ 1_introducao.m
@@ -0,0 +1,20 @@
+pkg load control
+#H = 2s / s^ + 2s + 100
+
+# Ficar atendo com o zero que precisa ser colocado.
+sinal = tf([2 0], [1 2 100])
+ts= 0.1
+z_zoh_sinal = c2d(sinal, ts, 'zoh')
+z_tustin_sinal = c2d(sinal, ts, 'tustin')
+
+# Quando colocamos o periodo de amostragem o octave/matlab já retorna pra gente
+# um sinal discreto em Z
+H = tf([1 -1],[1 1 0.3],0.1)
+# Para converter de volta usamos
+Hc = d2c(H)
+
+# zpk - Zero Pole Gain!!!!! Essa função parece não estar funcionando no octave, só n
+zpk(z_zoh_sinal);
+zpk(z_tustin_sinal);
+
+
---
+++ 2_introducao.m
@@ -0,0 +1,36 @@
+# Memória:
+#    Diz-se que um sistema possui memória se sua 
+#    saída depender de valores passados do sinal
+#    de entrada.
+# Exemplo: Moving Average
+
+# Causalidade:
+#     Diz-se que um sistema é causal se o valor
+#     atual do sinal de saída depender somente dos
+#     valores presentes e/ou passados do sinal de
+#     entrada.
+
+# Invertibilidade
+#     Diz-se que um sistema é invertível se a entrada
+#     do sistema puder ser recuperada através do
+#     sinal de saída do sistema
+#     
+#     Uma característica importante associada aos
+#     sistemas invertíveis é que entradas distintas
+#     devem produzir saídas distintas.
+
+# Invariância no Tempo
+#     Um sistema é dito invariante no tempo se um
+#     deslocamento no tempo do sinal de entrada
+#     (retardo ou avanço) implicar em um
+#     deslocamento temporal idêntico no sinal de
+#     saída.
+
+# Linearidade
+#     Tal propriedade também aplica-se aos
+#     sistemas, caracterizando-os como lineares.
+#     Sendo assim, a resposta de um sistema linear a
+#     uma soma ponderada de sinais de entrada é
+#     igual à mesma soma ponderada dos sinais de
+#     saída associados a cada um dos respectivos
+#     sinais de entrada.
