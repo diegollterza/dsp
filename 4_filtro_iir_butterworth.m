06aed9f51c71e6189b5405f4992b442d30e37c03
Luis Felipe Mileo <mileo@kmee.com.br>
2018-12-10 11:38:43 -0200

    [NEW] Butterworth
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ 4_filtro_iir_butterworth.m
@@ -0,0 +1,65 @@
+# Projeto de filtros IIR de tempo discreto a partir de filtros de tempo contínuo
+
+# The Scientist and Engineer's Guide to Digital Signal Processing – Steven W. Smith Capítulo 20
+# http://www.dspguide.com/ch20.htm
+
+# - A arte do projeto de filtro IIR de tempo contínuo está altamente avançada.
+# - Muitos métodos úteis de projeto IIR de tempo contínuo possuem fórmulas 
+#       fechadas de projeto relativamente simples.
+# - Os métodos de aproximação padrão não levam a fórmulas de projeto fechadas 
+#       simples quando esses métodos são aplicados diretamente ao caso IIR de 
+#       tempo discreto.
+
+# Filtro Passa-baixas Butterworth
+
+# - Filtro com resposta em magnitude maximamente plana na banda de passagem
+# - Descrito pela primeira vez em 1930 pelo engenheiro e físico 
+#     Stephen Butterworth em seu artigo “On the theory of filter amplifiers”
+# - À época de seu desenvolvimento, o projeto de filtros dependia muito da 
+#     experiência e da intuição do engenheiro devido à limitação da teoria e,
+#     geralmente, produzia filtros com muito ripple na banda de passagem.
+# - O filtro só se tornou de uso comum 30 anos depois de sua publicação
+
+# No livro vamos ver que é muito comum usar tabelas, mas se você tiver uma base
+# matématica vai perceber que é possível ter um precisão maior fazendo os 
+# cálculos manualmente:
+
+# Por exemplo, para o projeto de um filtro passa baixa:
+# - Banda de passagem até 100Hz com 1dB de ripple;
+# - Atenuação de -15dB a partir de 400Hz;
+# - Rp = -1dB e Wp=2π100 rad/s
+# - Rs = -15dB e Ws=2π400 rad/s
+format long;
+pkg load control;
+pkg load signal;
+T=1/5000;
+fs=1/T;
+t=0:T:1-T;
+
+sinal=sin(1*pi*200*t)+sin(1*pi*400*t)+sin(1*pi*600*t)+sin(1*pi*800*t)+sin(1*pi*1000*t)+sin(1*pi*1200*t)+sin(1*pi*1400*t)+sin(1*pi*1600*t)+sin(1*pi*1800*t);
+
+H = iir_buttlerworth_base(-1, 100, -15, 400)
+Hz=c2d(H,T, 'zoh');
+# Nao consegui uma forma de converter a transformada Z em uma equação de diferenaças
+# Vou montar o algoritimo do filtro manualmente e depois sobrando um tempo volto nisso.
+z = tf([1],[1 0], T)
+[Nd,Dd]=tfdata(Hz,'v');
+dsp_z = filt(Nd, Dd)
+
+N=length(sinal);
+y=zeros(N);
+y(1) = 0.01313*sinal(1);
+y(2) = 1.752*y(2-1) + 0.01427*sinal(2-1)  + 0.01313*sinal(2);
+for k=3:N 
+    y(k) = 1.752*y(k-1) - 0.7745*y(k-2) + 0.01427*sinal(k-1) + 0.01313*sinal(k);
+endfor
+
+figure(1), espectro(sinal, 1/T);
+figure(2), espectro(y, 1/T);
+
+
+
+#1) Projetar (algoritmo) um filtro IIR Butterworth para eliminar as componentes inferiores a 1,2kHz.
+#2) Projetar (algoritmo) um filtro IIR Butterworth para eliminar as componentes superiores a 1kHz.
+#3) Projetar (algoritmo) um filtro IIR Butterworth para eliminar as componentes de 600Hz e 800 Hz.
+#4) Projetar (algoritmo) um filtro IIR Butterworth para eliminar todas as componentes, deixando apenas a de 1,4kHz.
---
+++ iir_buttlerworth_base.m
@@ -0,0 +1,28 @@
+function [y] = iir_buttlerworth_base(ganho_db_p, fc_p, ganho_db_s, fc_s)
+  pkg load control
+  ganho_p = db2mag(ganho_db_p);
+  ganho_s = db2mag(ganho_db_s);
+  wp = 2*fc_p*pi;
+  ws = 2*fc_s*pi;
+  # Ficar muito atendo com esses parenteses pois levei quase 1 hora para acertar a conta
+  # :@
+  n = log10((1/ganho_p^2-1)/(1/ganho_s^2-1)) / ( 2*log10((wp/ws)) );
+  n = ceil(n);
+  omega_c = wp*(1/(1/ganho_p^2-1))^(1/(2*n));
+  display(omega_c/(2*pi));
+
+  m=1;
+  for k=0:2*n-1
+    polo=omega_c*e^(((i*pi)/(2*n))*((2*k)+1));
+    if real(polo)<0
+      rp(m)=polo;
+      m=m+1;
+    endif
+  endfor 
+  disp('Polos estáveis: ');
+  disp(rp');
+  
+  H=tf(omega_c^n,real(poly(rp)));
+  y=zpk(H);
+  
+endfunction
