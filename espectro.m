0d44f6f6e1d27b67a1bcd888219c25d7412c83e6
Luis Felipe Mileo <mileo@kmee.com.br>
2018-12-12 12:32:43 -0200

    [IMP] Espectro
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- espectro.m
+++ espectro.m
@@ -1,19 +1,15 @@
 function [freq, amp, fase] = espectro(sinal, fs)
-  % Quantidade de pontos de um sinal;
   N = length(sinal);
-  
   df = fs/N;
-  # Vetor de frequencias
   freq = 0:df:fs-df;
   
   Esinal=fft(sinal);
-  subplot(2, 1, 1);
+  amp = 2 * abs(Esinal(1:floor(N/2)))/N;
+  fase = 180/pi * angle(Esinal(1:floor(N/2)));
   
-  stem(freq(1:floor(N/2)), 2*abs(Esinal(1:floor(N/2)))/N);
-  grid on;
-  
-  subplot(2,1,2);
-  stem(freq(1:floor(N/2)), angle(Esinal(1:floor(N/2)))*180/pi);
-  grid on;
+  subplot(2, 1, 1); 
+  stem(freq(1:floor(N/2)), amp); grid on;
+  subplot(2,1,2); 
+  stem(freq(1:floor(N/2)), fase); grid on;
 endfunction
 
